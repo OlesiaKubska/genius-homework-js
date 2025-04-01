c94c407d5d44abe89a92f6c980aaed36ebf21008
OlesiaKubska <kublesia0908@gmail.com>
2025-04-01 12:39:30 +0200

    Added files hw-08

---
+++ hw-08/script.js
@@ -0,0 +1,13 @@
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// <!DOCTYPE html>
+// <html>
+// <body>
+// <div data-widget-name="menu">Виберіть жанр</div>
+// <script>/* ваш код */</script>
+// </body>
+// </html>
+
+
+const element = document.querySelector("[data-widget-name]");
+console.log(element.dataset.widgetName); // Вив: menu
